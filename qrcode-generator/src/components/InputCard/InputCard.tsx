12d031a5c84170ea9d8b5ff17f3e55d9c57d7a11
ShreyaPrasad1209 <shreyapd06@gmail.com>
2022-10-29 20:49:20 +0530

    feat: api request to QR code API

--- qrcode-generator/src/components/InputCard/InputCard.tsx
+++ qrcode-generator/src/components/InputCard/InputCard.tsx
@@ -1,11 +1,27 @@
-import { InputCardContainer, InputCardTitle, InputCardInput, InputCardButton } from "./InputCard.style";
+import {
+  InputCardContainer,
+  InputCardTitle,
+  InputCardInput,
+  InputCardButton,
+} from "./InputCard.style";
 
-export default function InputCard() {
+interface InputCardProps {
+  generateQRCode: (qrVal: string) => void;
+  qrVal: string;
+}
+
+export default function InputCard({ generateQRCode, qrVal }: InputCardProps) {
+  const handleOnClick = () => {
+    generateQRCode(qrVal);
+  };
+
   return (
     <InputCardContainer>
-      <InputCardTitle>Enter your URL here </InputCardTitle>
+      <InputCardTitle> Enter your URL here </InputCardTitle>
       <InputCardInput />
-      <InputCardButton>Generate QR Code</InputCardButton>
+      <InputCardButton onClick={handleOnClick} qrVal = {InputCardInput.value}>
+        Generate QR Code
+      </InputCardButton>
     </InputCardContainer>
   );
 }
