777cd1c4577bd998f177d5e52e401652ed06abe8
Nitin Kumar <snitin315@gmail.com>
2022-10-30 19:40:58 +0530

    fix: QR code implementation

--- qrcode-generator/src/components/InputCard/InputCard.tsx
+++ qrcode-generator/src/components/InputCard/InputCard.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import {
   InputCardContainer,
   InputCardTitle,
@@ -5,23 +6,25 @@ import {
   InputCardButton,
 } from "./InputCard.style";
 
-interface InputCardProps {
-  generateQRCode: (qrVal: string) => void;
-  qrVal: string;
-}
-
-export default function InputCard({ generateQRCode, qrVal }: InputCardProps) {
-  const handleOnClick = () => {
-    generateQRCode(qrVal);
-  };
+export default function InputCard() {
+  const inputRef = useRef({} as HTMLInputElement);
+  const [qrCodeValue, setQrCodeValue] = useState("");
 
   return (
-    <InputCardContainer>
-      <InputCardTitle> Enter your URL here </InputCardTitle>
-      <InputCardInput />
-      <InputCardButton onClick={handleOnClick} qrVal = {InputCardInput.value}>
-        Generate QR Code
-      </InputCardButton>
-    </InputCardContainer>
+    <>
+      <InputCardContainer>
+        <InputCardTitle> Enter your URL here </InputCardTitle>
+        <InputCardInput ref={inputRef} />
+        <InputCardButton onClick={() => setQrCodeValue(inputRef.current.value)}>
+          Generate QR Code
+        </InputCardButton>
+      </InputCardContainer>
+      {qrCodeValue && (
+        <img
+          src={`https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrCodeValue}`}
+          alt={`QR Code for the entered URL ${qrCodeValue}`}
+        />
+      )}
+    </>
   );
 }
